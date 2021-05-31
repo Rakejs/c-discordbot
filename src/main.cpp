755a68ed1152a186416dfdebc57a3295c376a1ec
Rakejs <raspdevpy@gmail.com>
2021-05-31 16:02:17 +0000

    add memeber count

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <sstream>
 #include <dpp/discordevents.h>
 #include <dpp/message.h>
+#include <string>
 #include <templatebot/templatebot.h>
 using namespace std;
 using json = nlohmann::json;
@@ -28,10 +29,13 @@ int main(int argc, char const *argv[])
             bot.message_create(dpp::message(event.msg->channel_id, e));
         }
         if (event.msg->content == "!stats") {
+            dpp::guild* msg_guild = dpp::find_guild(event.msg->guild_id);
             dpp::embed e;
             e.set_title("Server Stats");
             e.set_color(148225192);
             e.set_description("The command views general server stats");
+            string memcount = to_string(msg_guild->member_count);
+            e.add_field("Memberscount",memcount,true);
             bot.message_create(dpp::message(event.msg->channel_id, e));
         }
     });
